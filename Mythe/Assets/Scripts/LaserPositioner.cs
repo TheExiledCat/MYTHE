300e54251ef8e634f100a44a11aaa2372169bc05
TheExiledCat <43574582+TheExiledCat@users.noreply.github.com>
2020-05-07 12:45:01 +0200

    laser feature working

---
+++ Mythe/Assets/Scripts/LaserPositioner.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserPositioner : MonoBehaviour
+{
+   
+     public Transform[] points;
+    [SerializeField]
+    private LineRenderer lr;
+    int length;
+    bool even;
+    void Start()
+    {
+        
+        length = points.Length;
+        if (length == 0)
+        {
+            enabled = false;
+        }
+        if (length % 2 == 0)//check if its an even number or not
+        {
+            lr.positionCount = length;
+            even = true;
+        }else
+        {
+            lr.positionCount = length + 1;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        for (int i = 0; i < length; i++)
+        {
+           
+            
+            
+                lr.SetPosition(i, points[i].position);
+            
+        }
+        if (!even)
+        {
+            lr.SetPosition(length, points[0].position);
+        }
+        
+    }
+}
