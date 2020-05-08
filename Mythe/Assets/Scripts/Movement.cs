e9c5bd7dd1ef706c1cb53752e57f0110a5d24b7e
HBlueRainDrop <43329986+HBlueRainDrop@users.noreply.github.com>
2020-05-08 10:58:50 +0200

    SnakeMovement
    First version of movement

---
+++ Mythe/Assets/Scripts/Movement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [Header("Movements")]
+    public float playerSpeed;
+    public float jumpForce;
+
+    public Rigidbody snakehead;
+
+    Vector3 x, z;
+
+    void Start()
+    {
+        snakehead = GetComponent<Rigidbody>();
+
+    }
+
+    void Update()
+    {
+        //-----Inputs-----//
+        x = transform.right * Input.GetAxisRaw("Horizontal");
+        z = transform.forward * Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            Jump();
+    }
+
+    void FixedUpdate()
+    {
+        Movements();
+    }
+
+    void Movements()
+    {
+        Vector3 movement = (x + z).normalized * playerSpeed;
+        snakehead.AddForce(movement);
+    }
+
+    void Jump()
+    {
+        snakehead.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+
+}
