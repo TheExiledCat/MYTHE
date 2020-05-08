7800bfa3fab4b4583ed1c0820ca246ba79991645
TheExiledCat <43574582+TheExiledCat@users.noreply.github.com>
2020-05-08 13:35:38 +0200

    added stamina

---
+++ Mythe/Assets/Scripts/Resource.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resource 
+{
+    protected float value;
+    protected float maxValue;
+    
+    public void Init()
+    {
+        value = maxValue;
+    }
+    public virtual void StartDepleting(float amount)
+    {
+        value = value -amount < 0 ?  0:value-amount;
+        if (value <= 0)
+        {
+            StopDepleting();
+        }
+    }
+    public virtual void StopDepleting()
+    {
+
+    }
+    public void SetMax(float amount)
+    {
+        maxValue = amount;
+    }
+    public float GetValue()
+    {
+        return value;
+    }
+}
