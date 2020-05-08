d90d5e1c3616c6e9f02f1fe2834412bdaacdfc93
TheExiledCat <43574582+TheExiledCat@users.noreply.github.com>
2020-05-08 13:34:47 +0200

    Revert "Stamina meter finished"
    This reverts commit 3d4250f6ddf6bce3627ff034642aae22861416fd.

--- Mythe/Assets/Scripts/Constants.cs
+++ Mythe/Assets/Scripts/Constants.cs
@@ -7,5 +7,4 @@ public class Constants
     public const int PLAYER_LAYER=1<<8;//8 and up are self made layers
     public const string PLAYER_TAG = "Player";
     public const int PLAYER_MAX_HEALTH=1;
-    public const float RESOURCE_MAX=100;
 }
