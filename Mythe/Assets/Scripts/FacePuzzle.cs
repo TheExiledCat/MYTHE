10c860981fb07d86b80850d60a3aa0a565cc6eb1
Falkra <35593043+Falkra@users.noreply.github.com>
2020-05-11 12:15:43 +0200

    Quick 1st puzzel test
    Added: FacePuzzelTest Scene
    Added: FacePuzzle Script (Moet verder uitgewerkt worden)
    Added: Temporary ColliderCheck Script
    Added: Extra Material for indication
    
    Notes:
    
    - Alles in temp kan verwijderd worden wanneer je wilt, ze worden wel gebruikt voor testing, dus vraag het eerst
    - De puzzel word verder uitgewerkt zodra ik er hulp bij krijg (Herbert helpt me er al mee, maar moest weg)

---
+++ Mythe/Assets/Scripts/FacePuzzle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FacePuzzle : MonoBehaviour
+{
+    public int faceOrder = 4;
+    public GameObject[] selectorArr;
+
+    void Start()
+    {
+        selectorArr = new GameObject[faceOrder];
+
+        for (int i = 0; i < faceOrder; i++)
+        {
+
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log("oof");
+        }
+    }
+}
