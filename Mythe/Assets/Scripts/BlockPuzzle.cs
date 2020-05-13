ca86e400b6ed2fbcddf143db11ffc0e63d4ceedc
TheExiledCat <43574582+TheExiledCat@users.noreply.github.com>
2020-05-13 17:19:21 +0200

    fixed missing files

---
+++ Mythe/Assets/Scripts/BlockPuzzle.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockPuzzle : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] blocks;
+    Collider[] cols;
+    // Start is called before the first frame update
+    [SerializeField]
+    Vector3 origin, extents;
+    [SerializeField]
+    Gate gate;
+    int numberOfBlocks = 0;
+    [SerializeField]
+    Pickup p;
+    // Update is called once per frame
+    void Update()
+    {
+        numberOfBlocks = 0;
+        cols = Physics.OverlapBox(origin, extents, Quaternion.identity, Constants.SELECTABLE_LAYER);
+        foreach (GameObject g in blocks)
+        {
+            if (ContainsBlock(g))
+            {
+                numberOfBlocks++;
+
+            }
+        }
+        if (numberOfBlocks == blocks.Length && p.selected == null)
+        {
+            Finish();
+            enabled = false;
+        }
+
+    }
+    void Finish()
+    {
+        foreach (GameObject g in blocks)
+        {
+            g.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
+            g.GetComponent<Rigidbody>().freezeRotation = true;
+            g.GetComponent<Selectable>().enabled = false;
+        }
+        gate.Open();
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.matrix = transform.localToWorldMatrix;
+        Gizmos.DrawWireCube(origin, extents);
+    }
+    bool ContainsBlock(GameObject b)
+    {
+
+
+        for (int j = 0; j < cols.Length; j++)
+        {
+            if (cols[j].gameObject == b)
+            {
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ Mythe/Assets/Scripts/Pickup.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    Ray ray;
+    RaycastHit hit;
+    [SerializeField]
+    float maxRange;
+
+    public GameObject selected;
+    float dist;
+    [SerializeField]
+    LayerMask selectables;
+    bool frame = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, maxRange, selectables))
+        {
+            if (hit.collider.GetComponent<Selectable>() != null && Input.GetMouseButtonDown(0) && selected == null)
+            {
+                selected = hit.collider.gameObject;
+                dist = Vector3.Distance(Camera.main.transform.position, selected.transform.position);
+                frame = true;
+            }
+        }
+        print(frame);
+        if (selected != null)
+        {
+            selected.transform.position = Camera.main.transform.position + Camera.main.transform.forward * dist;
+            if (Input.GetMouseButtonDown(0) && !frame)
+            {
+                selected = null;
+            }
+            frame = false;
+        }
+
+
+
+        Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * maxRange, Color.red);
+        print(selected);
+
+    }
+
+}
