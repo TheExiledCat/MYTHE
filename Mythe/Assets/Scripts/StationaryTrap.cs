89ae129a208b9ba4b8043ee1f1488a056d2b7103
TheExiledCat <43574582+TheExiledCat@users.noreply.github.com>
2020-05-13 13:14:09 +0200

    Revert "meeste van het level af"
    This reverts commit 3f3a3496f91a7e7d119326fb8248c7238ff64d48.

--- Mythe/Assets/Scripts/StationaryTrap.cs
+++ Mythe/Assets/Scripts/StationaryTrap.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[RequireComponent(typeof(BoxCollider))]
 public class StationaryTrap : Trap
 {
     [SerializeField]
