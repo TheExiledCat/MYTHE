9911740907be39941a67e4723e469025395bd0c9
TheExiledCat <43574582+TheExiledCat@users.noreply.github.com>
2020-05-14 16:59:48 +0200

    scenery and traps merged

---
+++ Mythe/Assets/Scripts/MovingTrap.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(MeshCollider))]
+public class MovingTrap : Trap
+{
+    void OnTriggerEnter(Collider col)
+    {
+        DamageCollider(col);
+    }
+}
