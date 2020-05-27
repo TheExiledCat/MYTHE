fe99dae0180fef45bbdacb0acbc365137dc2a814
TheExiledCat <43574582+TheExiledCat@users.noreply.github.com>
2020-05-27 23:08:35 +0200

    fix merge error

--- Mythe/Assets/Scripts/Gate.cs
+++ Mythe/Assets/Scripts/Gate.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class Gate : MonoBehaviour
 {
+    public AudioSource Gatesound;
     public void Open()
     {
+
         StartCoroutine(MoveGate());
     }
     IEnumerator MoveGate()
     {
         for (int i = 0; i < 60; i++)
         {
+            Gatesound.Play(0);
             transform.position += Vector3.down * (transform.localScale.y / 2);
             yield return new WaitForEndOfFrame();
         }
