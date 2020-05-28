640cfbbd698ae49fc0f8a0f3f72ff2435aa49691
Falkra <justinudo959@gmail.com>
2020-05-28 17:56:33 +0200

    DialogueSystem
    Added: 4 scripts
    - Dialogues
    - DialogueCycle
    - DialogueTriggers
    - DialogueDisplay
    
    Added: Scene: DisplayTest

---
+++ Mythe/Assets/Scripts/DialougScripts/DialogueDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogueDisplay : MonoBehaviour
+{
+    public TextMeshProUGUI txt;
+
+    public void ShowDialogue(string text)
+    {
+        StopDialogue();
+        txt.gameObject.SetActive(true);
+        txt.text = text;
+        Invoke("StopDialogue", 4f);
+
+    }
+
+    void StopDialogue()
+    {
+        txt.gameObject.SetActive(false);
+    }
+}
---
+++ Mythe/Assets/Scripts/DialougScripts/DialogueTriggers.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTriggers : MonoBehaviour
+{
+    DialogueCycle dCycle;
+    public string key;
+
+    void Start()
+    {
+        dCycle = GameObject.FindGameObjectWithTag("Player").GetComponent<DialogueCycle>();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.CompareTag("Player"))
+        {
+            dCycle.Cycle(key);
+        }
+        print(col);
+    }
+}
